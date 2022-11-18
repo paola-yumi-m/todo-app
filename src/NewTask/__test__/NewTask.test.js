a36dde776b97e7880b75de9b3c8d0059403fbae8
paola-yumi-m <paola.yumi.m@gmail.com>
2022-11-18 09:04:19 -0300

    Testando componente NewTask.

--- src/NewTask/__test__/NewTask.test.js
+++ src/NewTask/__test__/NewTask.test.js
@@ -1,6 +1,7 @@
 import {fireEvent, render, screen} from "@testing-library/react";
 import '@testing-library/jest-dom';
 import {NewTask} from "../NewTask";
+import userEvent from "@testing-library/user-event";
 
 describe('NewTask', () => {
     it('should render input element', () => {
@@ -20,22 +21,22 @@ describe('NewTask', () => {
         expect(inputElement).toBeInTheDocument();
     });
 
-    it('should be able to type in input', function () {
+    it('should be able to type in input',  () => {
         const mockedSetTaskList = jest.fn();
         const mockedSetNewTask = jest.fn();
         const newTask = {name: '', id: 0};
+
         render(
             <NewTask
                 taskList={['Do homework', 'Clean house', 'Feed the dog']}
                 setTaskList={mockedSetTaskList}
-                newTask={newTask}
                 setNewTask={mockedSetNewTask}
+                newTask={newTask}
                 lastId={0}
             />
         );
         const inputElement = screen.getByLabelText('Task Name');
-        fireEvent.change(inputElement, {target: {value: 'Walk on the beach'}});
-        screen.debug();
-        expect(inputElement.value).toBe('Walk on the beach');
+        userEvent.type(inputElement, 'Go grocery shopping')
+        expect(inputElement).toHaveValue('Walk on the beach');
     });
 })
