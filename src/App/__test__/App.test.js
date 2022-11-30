7135917fbb5b82a2e2dc09bdbbc7e6a31a9255d1
paola-yumi-m <paola.yumi.m@gmail.com>
2022-11-30 08:39:45 -0300

    Testando componente App

--- src/App/__test__/App.test.js
+++ src/App/__test__/App.test.js
@@ -1,4 +1,4 @@
-import {fireEvent, prettyDOM, render, screen} from "@testing-library/react";
+import {fireEvent, render, screen} from "@testing-library/react";
 import {App} from "../App";
 import userEvent from "@testing-library/user-event";
 
@@ -42,5 +42,48 @@ describe('App', function () {
         const taskList = screen.getAllByTestId('span');
         expect(taskList.length).toBe(4);
         expect(taskList[3].innerHTML).toBe('Do homework');
+    });
+
+    it('should add task to Completed Tasks when checkbox is checked', function () {
+        render(
+            <App />
+        );
+
+        const checkboxes = screen.getAllByRole('checkbox');
+        fireEvent.click(checkboxes[0]);
+        fireEvent.click(checkboxes[2]);
+        const completedTasks = screen.getAllByRole('listitem');
+        expect(completedTasks.length).toBe(2);
+        expect(completedTasks[0].innerHTML).toBe('Task 1');
+        expect(completedTasks[1].innerHTML).toBe('Task 3');
+    });
+
+    it('should remove task of Completed Tasks when checkbox is unchecked', function () {
+        render(
+            <App />
+        );
+
+        const checkboxes = screen.getAllByRole('checkbox');
+        fireEvent.click(checkboxes[0]);
+        fireEvent.click(checkboxes[0]);
+        fireEvent.click(checkboxes[1]);
+        fireEvent.click(checkboxes[2]);
+        fireEvent.click(checkboxes[2]);
+        const completedTasks = screen.getAllByRole('listitem');
+        expect(completedTasks.length).toBe(1)
+        expect(completedTasks[0].innerHTML).toBe('Task 2');
+    });
+
+    it('should remove task from TaskList when delete button is clicked', function () {
+        render(
+            <App />
+        );
+
+        const deleteButtons = screen.getAllByTestId('delete');
+        fireEvent.click(deleteButtons[0]);
+        fireEvent.click(deleteButtons[2]);
+        const taskList = screen.getAllByTestId('span');
+        expect(taskList.length).toBe(1);
+        expect(taskList[0].innerHTML).toBe('Task 2');
     });
 });
